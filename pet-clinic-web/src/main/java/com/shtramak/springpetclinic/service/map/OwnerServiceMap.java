85cecd34cd04702d96e4facff310e8b569db9cc9
Andrii Kovalchuk <shtramak@gmail.com>
2019-11-19 17:07:15 +0200

    move service package to pet-clinic-web package

--- pet-clinic-web/src/main/java/com/shtramak/springpetclinic/service/map/OwnerServiceMap.java
+++ pet-clinic-web/src/main/java/com/shtramak/springpetclinic/service/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package com.shtramak.springpetclinic.service.map;
 
 import com.shtramak.springpetclinic.model.Owner;
 import com.shtramak.springpetclinic.service.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Optional<Owner> findByLastName(String lastName) {
--- pet-clinic-web/src/main/java/com/shtramak/springpetclinic/service/map/PetServiceMap.java
+++ pet-clinic-web/src/main/java/com/shtramak/springpetclinic/service/map/PetServiceMap.java
@@ -1,7 +1,9 @@
 package com.shtramak.springpetclinic.service.map;
 
 import com.shtramak.springpetclinic.model.Pet;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> {
     @Override
     public Pet save(Pet pet) {
