2752466c809f7e849575c4d959537431441806a3
shtramak <shtramak@gmail.com>
2019-11-20 23:04:39 +0200

    refactor PetServiceMap and VetServiceMap

--- pet-clinic-web/src/main/java/com/shtramak/springpetclinic/service/map/PetServiceMap.java
+++ pet-clinic-web/src/main/java/com/shtramak/springpetclinic/service/map/PetServiceMap.java
@@ -1,10 +1,11 @@
 package com.shtramak.springpetclinic.service.map;
 
 import com.shtramak.springpetclinic.model.Pet;
+import com.shtramak.springpetclinic.service.PetService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Pet save(Pet pet) {
         super.saveOrUpdate(pet.getId(), pet);
--- pet-clinic-web/src/main/java/com/shtramak/springpetclinic/service/map/VetServiceMap.java
+++ pet-clinic-web/src/main/java/com/shtramak/springpetclinic/service/map/VetServiceMap.java
@@ -1,10 +1,11 @@
 package com.shtramak.springpetclinic.service.map;
 
 import com.shtramak.springpetclinic.model.Vet;
+import com.shtramak.springpetclinic.service.VetService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class VetServiceMap extends AbstractMapService<Vet,Long> {
+public class VetServiceMap extends AbstractMapService<Vet,Long>  implements VetService {
     @Override
     public Vet save(Vet vet) {
         return super.saveOrUpdate(vet.getId(), vet);
