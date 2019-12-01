007cc3bc8d292615b010859ea1e6a7637dbe8294
Andrii Kovalchuk <shtramak@gmail.com>
2019-12-01 23:09:41 +0200

    add Contact Info Properties to Owners. Close #27

--- pet-clinic-data/src/main/java/com/shtramak/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/shtramak/springpetclinic/model/Vet.java
@@ -8,5 +8,8 @@ import java.util.Set;
 @Getter
 @Setter
 public class Vet extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Speciality> specialities;
 }
