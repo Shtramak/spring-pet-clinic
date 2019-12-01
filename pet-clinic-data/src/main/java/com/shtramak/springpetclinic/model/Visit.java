e7fe37555e7197fa61bbf93c3f572574d4f8ec70
Andrii Kovalchuk <shtramak@gmail.com>
2019-12-01 22:58:48 +0200

    add visits, owner with pets relationship. Close #25

---
+++ pet-clinic-data/src/main/java/com/shtramak/springpetclinic/model/Visit.java
@@ -0,0 +1,14 @@
+package com.shtramak.springpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class Visit extends BaseEntity {
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+}
