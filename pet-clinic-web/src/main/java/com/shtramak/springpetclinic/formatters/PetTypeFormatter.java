e22651078cbc9257a3cce177163f92a1b93f5b10
Andrii Kovalchuk <shtramak@gmail.com>
2020-01-14 17:25:51 +0200

    fix issue with Pet edit

---
+++ pet-clinic-web/src/main/java/com/shtramak/springpetclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,41 @@
+package com.shtramak.springpetclinic.formatters;
+
+import com.shtramak.springpetclinic.model.PetType;
+import com.shtramak.springpetclinic.service.PetTypeService;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+/**
+ * Created by jt on 9/22/18.
+ */
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+}
