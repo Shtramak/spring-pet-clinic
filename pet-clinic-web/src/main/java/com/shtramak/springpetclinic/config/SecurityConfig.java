55789d342459de9ed4e71daa6fb6bb461a078625
Andrii Kovalchuk <shtramak@gmail.com>
2020-01-20 16:48:42 +0200

    add Spring Security dependencies and basic configuration

---
+++ pet-clinic-web/src/main/java/com/shtramak/springpetclinic/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.shtramak.springpetclinic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder user = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(user.username("John Doe").password("password").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll();
+    }
+}
